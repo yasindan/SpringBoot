6aa6c1ae5f82fc4c5e70a5990bfd084fbfae5679
单愈 <yu.shan@hand-china.com>
2018-11-15 10:46:15 +0800

    [update] 修改user服务。

--- user/src/main/java/com/sy/user/controller/RedisController.java
+++ user/src/main/java/com/sy/user/controller/RedisController.java
@@ -2,9 +2,7 @@ package com.sy.user.controller;
 
 import com.sy.user.dto.User;
 import com.sy.user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -13,10 +11,17 @@ import javax.annotation.Resource;
 public class RedisController {
     @Resource
     private UserService userService;
-    @GetMapping("set")
+    @GetMapping("/set/")
     public void set() {
-        userService.set("key1", new User(1, "xiaoxi", 26));
+        userService.set("key1", new User(1, "sy", 26));
     }
+
+    @RequestMapping(value = {"/setKey"}, method = RequestMethod.POST)
+    @ResponseBody
+    public void setKey(@RequestBody String key,@RequestBody User user){
+        userService.set(key, user);
+    }
+
     @GetMapping("get")
     public String get() {
         return userService.get("key1").getName();
@@ -25,6 +30,14 @@ public class RedisController {
     public void stringset() {
         userService.setCode("stringkey", "guoxiaoxi");
     }
+
+
+    @RequestMapping(value = {"/setKey/{key}/{value}"}, method = RequestMethod.GET)
+    @ResponseBody
+    public void stringsetKey(@PathVariable String key,@PathVariable String value) {
+        userService.setCode(key, value);
+    }
+
     @GetMapping("stringget")
     public String stringget() {
         return userService.getCode("stringkey");
